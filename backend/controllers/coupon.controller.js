d4161bdde3ad419b407ad4303895889fe801ef03
Vivek Kumar Barnwal <vivekbarnwal2001@gmail.com>
2025-06-06 13:48:23 +0530

    implement Cart routes and Coupon routes

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,33 @@
+export const getCoupon = async(req, res)=>{
+    try {
+        const coupon = await Coupon.findOne({userId: req.user._id, isActive: true })
+        res.json(coupon || null);
+    } catch (error) {
+        console.log("Error in getCoupon controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+
+}
+
+export const validateCoupon = async(req , res)=>{
+    try {
+        const {code} =req.body;
+        if(!coupon){
+            return res.status(404).json({ message: "Coupon not found"});
+        }
+        if(coupon.expirationDate < new Date()){
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(404).json({ message: "Coupon Expired"});
+        }
+
+        res.josn({
+            message: "Coupon is valid",
+            code: coupon.code,
+            discountPercentage: coupon.discountPercentage
+        })
+    } catch (error) {
+        console.log("Error in validateCoupon controller ", error.message);
+        res.status(500).json({ message: "Server Error", error:error.message})
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.route.js'
 import productRoutes from './routes/product.route.js'
+import cartRoutes from './routes/cart.route.js'
+import couponRoutes from './routes/coupon.route.js'
+
+
 import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -16,6 +21,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes)
+app.use("/api/cart", cartRoutes)
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Server is running on http://localhost:" + PORT);
