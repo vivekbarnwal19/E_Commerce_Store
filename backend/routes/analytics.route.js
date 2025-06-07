14f9bf3ef9cc3ff417d16eb7ba871ccffa5fc862
Vivek Kumar Barnwal <vivekbarnwal2001@gmail.com>
2025-06-07 12:59:20 +0530

    implement the Analytics routes

---
+++ backend/routes/analytics.route.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
+import { getAnalyticsData, getDailySalesData } from "../controllers/analytics.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute,adminRoute, async(req , res) =>{
+    try {
+        const analyticsData = await getAnalyticsData();
+
+        const endDate =new Date();
+        const startDate =new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+        const dailySalesData = await getDailySalesData(startDate, endDate);
+
+        res.json({
+            analyticsData,
+            dailySalesData
+        })
+
+    } catch (error) {
+        
+    }
+})
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from './routes/product.route.js'
 import cartRoutes from './routes/cart.route.js'
 import couponRoutes from './routes/coupon.route.js'
 import paymentRoutes from './routes/payment.route.js'
+import anlyticsRoutes from './routes/analytics.route.js'
 
 
 import { connectDB } from './lib/db.js';
@@ -25,6 +26,7 @@ app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", anlyticsRoutes)
 
 app.listen(PORT, ()=>{
     console.log("Server is running on http://localhost:" + PORT);
